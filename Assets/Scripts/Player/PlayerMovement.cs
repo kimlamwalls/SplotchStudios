21f600b79eaf9281c17205ce7e00f4aa99bad333
Zed <s3775574@student.rmit.edu.au>
2022-04-05 14:10:54 +1000

    Directional player idling, small spider enemy
    - player now idles in the direction of their previous movement
    - started work on a small spider enemy
    - candle particles

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -13,11 +13,13 @@ public class PlayerMovement : MonoBehaviour
 
     // player movement vector
     Vector2 movement;
+    // the players last movement vector
+    Vector2 prevVector;
 
     AdventureLog log;
     PlayerHealthBar hb;
 
-    int count = 0;
+   
     
     void Awake()
     {
@@ -35,10 +37,34 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         // Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+  
+
+        Debug.Log($"x: {x}, y:{y}");
+
+        movement.x = x;
+        movement.y = y;
+
+
+        // when the player stops moving x and y reset to 0 which causes the down idle animation to play
+        // even though the player is facing a different direction, so we store the lastvector movement of the player
+        // and use that as the animation state, as this is only updated when the player moves, 
+        // the player now idles in the last direction they were moving
+        if (x != 0 || y != 0)
+        {
+            prevVector.x = x;
+            prevVector.y = y;    
+        }
+
+        // update animation state
+        animator.SetFloat("Horizontal", prevVector.x);
+        animator.SetFloat("Vertical", prevVector.y);
+
+
+
+
+
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
         // DEBUG :: REMOVE LATER
@@ -47,8 +73,8 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetTrigger("TakeDamage");
             hb.Damage(5);
-            log.AddEventMessage("Damage " + count);
-            count++;
+            log.AddEventMessage("Damage taken");
+            
         }
 
         // 'P' key is for attack
@@ -62,7 +88,7 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         // move player
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * movement);
     }
 
     void MeleeAttack()
