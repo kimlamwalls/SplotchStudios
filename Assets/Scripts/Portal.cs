058db3343c551c2d7db00fbe054146654991ac0a
Kim Walls <s3917984@student.rmit.edu.au>
2022-05-04 01:29:27 +1000

    Added thunder to boss room, moved portal to main room temporarily for debug

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,16 +1,64 @@
+using System;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using Random = UnityEngine.Random;
 
 public class Portal : Collidable
 {
     public string[] sceneNames;
+    public GameObject lighting;
+    private Light2D lightingComponent;
+    private double secondsToWait = 1.3f;
+    private float startIntensity;
+    private float brightnessOfFlash = 12;
+    private int flashCounter = 0;
 
-    protected override void OnCollide(Collider2D coll)
+   //Component dungeon lighting.GetComponent("light 2D");
+
+   private void Awake()
+   { 
+       lighting = GameObject.Find("Dungeon_Global_Light");
+     lightingComponent = lighting.GetComponent<Light2D>();
+   }
+
+   void Start()
+   {
+       //get the default lighting
+       startIntensity = lightingComponent.intensity;
+   }
+
+   private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (coll.name == "Player")
+        if (collider.gameObject.tag == "Player")
         {
             // Teleport the player 
             string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+
+        //invoke lighting flashes   
+             InvokeRepeating("Flash", 0.8f, 0.2f);
+             InvokeRepeating("TurnOffFlash", 0.9f, 0.2f);
+
         }
     }
+
+   void Update()
+   {
+       if (flashCounter >= 3) ;
+       {
+           lightingComponent.intensity = startIntensity;
+           CancelInvoke("Flash");
+       }
+   }
+   
+   void Flash()
+   {
+       lightingComponent.intensity = brightnessOfFlash;
+       flashCounter += 1;
+   }
+   
+   void TurnOffFlash()
+   {
+       lightingComponent.intensity = startIntensity;
+   }
 }
