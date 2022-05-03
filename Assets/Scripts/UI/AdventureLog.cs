17191b32ca4314b349c2501f44a962a61ad0a87d
Ari <s3933486@student.rmit.edu.au>
2022-05-03 17:09:48 +1000

    Implemented more trigger logs

--- Assets/Scripts/UI/AdventureLog.cs
+++ Assets/Scripts/UI/AdventureLog.cs
@@ -10,7 +10,7 @@ public class AdventureLog : MonoBehaviour
     public GameObject LogPanel;
     public GameObject textPrefab;
 
-    private readonly int MaxLogs = 5;
+    private readonly int MaxLogs = 2;
     public void AddEventMessage(string message)
     {
         if (Logs.Count >= MaxLogs)
